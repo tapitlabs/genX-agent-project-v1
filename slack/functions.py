4a47cef254599ea6d17d084198ccb804b0836542
tapitlabs <75966443+tapitlabs@users.noreply.github.com>
2025-03-16 02:36:24 -0400

    Update functions.py

--- slack/functions.py
+++ slack/functions.py
@@ -1,4 +1,4 @@
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import find_dotenv, load_dotenv
 from langchain.prompts.chat import (
